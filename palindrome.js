af9260b39776196bd739f9dc91488f33bd4d31b9
raotoqeer53 <raotoqeer53@icloud.com>
2022-12-18 17:26:02 +0500

    palindrome function done

---
+++ palindrome.js
@@ -0,0 +1,24 @@
+function checkPalindrome(string) {
+    
+    // find the length of a string
+    const len = string.length;
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+// take input
+
+const string = '12321';
+// const string = 'Pakistan';
+
+// call the function
+const value = checkPalindrome(string);
+
+console.log(value);
